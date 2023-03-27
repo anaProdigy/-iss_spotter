abcb0ccf5e56cb01d1c1ef448156e031f8d3d720
Anastasia <z.anastasia11@gmail.com>
2023-03-27 14:52:53 -0700

    Comment out the call to fetchCoordsByIP from index.js since we have completed our verification of it.

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 //   console.log('It worked! Returned IP:', ip);
 // });
 
-fetchCoordsByIP('50.68.205.27', (error, result) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
+// fetchCoordsByIP('50.68.205.27', (error, result) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
 
-  console.log('It worked! Returned data:', result);
-});
+//   console.log('It worked! Returned data:', result);
+// });
